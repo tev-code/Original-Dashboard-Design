bba61dc63b4a9ab5504cbb456f972e06a715b5e0
Tevin <tevin@trade245.com>
2023-05-19 12:34:15 +0200

    latest

--- script.js
+++ script.js
@@ -1,36 +1,61 @@
-// Generate random data
-function generateRandomData() {
-  return Array.from({ length: 7}, () => Math.floor(Math.random() * 100));
-}
+  // Generate random data
+  function generateRandomData() {
+  return Array.from({ length: 7 }, () => Math.floor(Math.random() * 50));
+  }
 
-// Chart configuration
-const chartConfig1 = {
-  type: 'bar',
-  data: {
+  // Chart configuration 1
+  const chartConfig1 = {
+    type: 'bar',
+    data: {
     labels: ['January', 'February', 'March', 'April', 'May', 'June', 'July'],
     datasets: [
-      {
-        label: 'Chart 2',
-        data: generateRandomData(),
-        backgroundColor: '#6b6b6b',
-        borderColor: 'rgba(255, 99, 132, 1)',
-        borderWidth: 1,
-      },
+    {
+    label: 'Chart 1',
+    data: generateRandomData(),
+    backgroundColor: '#b30631',
+    borderColor: 'rgba(255, 99, 132, 1)',
+    borderWidth: 1,
+    },
     ],
-  },
-  options: {
+    },
+    options: {
     responsive: true,
     scales: {
-      y: {
-        beginAtZero: true,
-      },
+    y: {
+    beginAtZero: true,
     },
+    },
+    },
+    };
+
+  // Chart configuration 2
+  const chartConfig2 = {
+  type: 'line',
+  data: {
+  labels: ['January', 'February', 'March', 'April', 'May', 'June', 'July'],
+  datasets: [
+  {
+  label: 'Chart 2',
+  data: generateRandomData(),
+  backgroundColor: '#b30631',
+  borderColor: 'rgba(255, 99, 132, 1)',
+  borderWidth: 1,
   },
-};
+  ],
+  },
+  options: {
+  responsive: true,
+  scales: {
+  y: {
+  beginAtZero: true,
+  },
+  },
+  },
+  };
 
-// Create the charts
-const ctx1 = document.getElementById('chart1').getContext('2d');
-const ctx2 = document.getElementById('chart2').getContext('2d');
+  // Create the charts
+  const ctx1 = document.getElementById('chart1').getContext('2d');
+  const ctx2 = document.getElementById('chart2').getContext('2d');
 
-new Chart(ctx1, chartConfig1);
-new Chart(ctx2, chartConfig2);
+  new Chart(ctx1, chartConfig1);
+  new Chart(ctx2, chartConfig2);
