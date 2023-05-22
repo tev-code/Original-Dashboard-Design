64de78737b20cd284476bd65db1035fa696e1071
Tevin <tevin@trade245.com>
2023-05-22 13:56:48 +0200

    latest

--- script.js
+++ script.js
@@ -12,8 +12,8 @@
     {
     label: 'Chart 1',
     data: generateRandomData(),
-    backgroundColor: '#b30631',
-    borderColor: 'rgba(255, 99, 132, 1)',
+    backgroundColor: '#8458B3',
+    borderColor: '#a28089',
     borderWidth: 1,
     },
     ],
@@ -37,8 +37,8 @@
   {
   label: 'Chart 2',
   data: generateRandomData(),
-  backgroundColor: '#b30631',
-  borderColor: 'rgba(255, 99, 132, 1)',
+  backgroundColor: '#8458B3',
+  borderColor: '#a28089',
   borderWidth: 1,
   },
   ],
